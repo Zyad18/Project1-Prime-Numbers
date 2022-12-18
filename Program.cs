9311e90986c2d6a606910241d7c49ae2d34cbd78
Zyad18 <117484572+Zyad18@users.noreply.github.com>
2022-12-19 00:19:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pls enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pls enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int  x ;
+            Console.Write("The prime numbers between "+n1+" and "+n2+" is: ");
+
+            for (int i = n1 ; i <= n2; i++) 
+            {
+               
+                for (x = 2 ; x <= i; x++)
+                {
+                    if ((i % x == 0))
+                        break;
+                 
+                }
+
+                if (x == i)  
+                    Console.Write( i + ", ");
+                
+            }
+
+
+        }
+    }
+}
